5997ee3780dc0f77337a2be1f898fe9666e95bf7
YOUHAMM17 <y.hammani02@gmail.com>
2025-06-05 11:13:21 -0400

    ajustement matrice

--- Coursera/cpp11.cpp
+++ Coursera/cpp11.cpp
@@ -3,7 +3,10 @@
 
 int main()
 {
-     std::vector<std::vector<double>> matrice { {1, 2, 3}, {4, 5, 6}, {7, 8, 9} };
+     std::vector<std::vector<double>> matrice { 
+     {1, 2, 3},
+     {4, 5, 6},
+     {7, 8, 9} };
 
      std::cout << "Bonjour, voici une matrice:" << std::endl;
      for (auto element : matrice) {
